2f5f4253b87f7f41be0c538414ba555ca34319b7
Wes Johnson <wes.johnson@fullscript.com>
2025-09-09 17:06:54 -0400

    ide config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "avoid",
+    rangeStart: 0,
+    rangeEnd: Infinity,
+  };
+  
